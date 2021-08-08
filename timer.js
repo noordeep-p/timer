848faa48f4d75acc15d05b4dc91c13dc718b0113
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-08 18:59:20 -0400

    refactor to include edge cases

--- timer.js
+++ timer.js
@@ -2,8 +2,10 @@ let input = process.argv.slice(2);
 
 const timer = function(input) {
   for (let i = 0; i < input.length; i++) {
-    setTimeout(() =>
-      process.stdout.write('beep'), Number(input[i] * 1000));
+    if (!(input[i] < 0 || isNaN(input[i]))) {
+      setTimeout(() =>
+        process.stdout.write('beep'), Number(input[i] * 1000));
+    }
   }
 };
 
